831b85de982dc9d823e451b6170bce34058983f8
erickalexander <erickcamacho1@gmail.com>
2018-01-08 14:02:59 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 // Code goes here
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import MovieCard from './components/Moviecard';
+
+const madMaxGenres = ['Action', 'Adventure', 'Science Fiction', 'Thriller'];
+
+ReactDOM.render(
+  <MovieCard
+    title="Mad Max: Fury Road"
+    poster="http://image.tmdb.org/t/p/w342/kqjL17yufvn9OVLyXYpvtyrFfak.jpg"
+    genres={madMaxGenres}
+  />,
+  document.getElementById('root')
+);
